04dc07117d749ff9fdb31a9597f1fb57d5ea3830
Jun4488 <96706340+Jun4488@users.noreply.github.com>
2023-03-16 16:56:10 +0800

    Add files via upload

---
+++ Training/项目管理/项目管理_项目开工会.mm
@@ -0,0 +1,258 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="项目开工会" FOLDED="false" ID="ID_809350591" CREATED="1666578574188" MODIFIED="1667262609940" LINK="质量管理.mm">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#fbf1c7">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_gruvbox_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_7974122" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_7974122" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc241d" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_103960811" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_103960811"/>
+<font SIZE="12" ITALIC="false"/>
+<edge COLOR="#cc241d"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#fdf6e3" BACKGROUND_COLOR="#282828" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#282828" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#282828" BACKGROUND_COLOR="#ebdbb2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#d79921" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffffff" BACKGROUND_COLOR="#b16286" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffffff" BACKGROUND_COLOR="#689d6a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffffff" BACKGROUND_COLOR="#a89984" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ebdbb2" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#ebdbb2">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#98971a">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="1.召集人" ID="ID_344908472" CREATED="1666578639331" MODIFIED="1666680857991">
+<node TEXT="Product Owner" ID="ID_1140416717" CREATED="1666579048556" MODIFIED="1666680613745"/>
+</node>
+<node TEXT="2.参会人员" ID="ID_365295575" CREATED="1666578764339" MODIFIED="1666680861337">
+<node TEXT="赞助人" ID="ID_278365930" CREATED="1666578770995" MODIFIED="1666682405324"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      公司内部：赞助人
+    </p>
+    <p>
+      公司外部：客户
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="核心团队" ID="ID_1482602198" CREATED="1666578799162" MODIFIED="1666682454308"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Product Owner：产品经理
+    </p>
+    <p>
+      Project Manager：项目经理，可由研发经理或开发经理承担
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="项目组成员" ID="ID_757584062" CREATED="1666578814283" MODIFIED="1666682485538"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      设计：
+    </p>
+    <p>
+      开发：
+    </p>
+    <p>
+      测试：
+    </p>
+    <p>
+      质量：
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="支撑团队人员" ID="ID_1650001971" CREATED="1666578829396" MODIFIED="1666682515522"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      市场：
+    </p>
+    <p>
+      服务：
+    </p>
+    <p>
+      部门职能经理：
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="案例" POSITION="left" ID="ID_1246654220" CREATED="1667814652122" MODIFIED="1667814660754">
+<node TEXT="僵木蠕项目" ID="ID_233566464" CREATED="1667814661270" MODIFIED="1667814685045" LINK="僵木蠕项目管理_项目开工会.mm"/>
+</node>
+<node TEXT="3.会议议题" POSITION="right" ID="ID_1633905305" CREATED="1666578686804" MODIFIED="1666680873927">
+<node TEXT="Product Owner" FOLDED="true" ID="ID_206831077" CREATED="1666678721027" MODIFIED="1667460510573" VGAP_QUANTITY="3 pt"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b style="font-weight: 600; color: rgb(18, 18, 18); font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif; font-size: medium; font-style: normal; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)"><font color="rgb(18, 18, 18)" face="-apple-system, BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif" size="medium">Product Owner （PO）</font></b><font color="rgb(18, 18, 18)" face="-apple-system, BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif" size="medium"><span style="color: rgb(18, 18, 18); font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, PingFang SC, Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif; font-size: medium; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255); display: inline !important; float: none">PO是了解客户需求以及相关商业价值的团队角色，作为客户代表，定义产品功能，决定产品发布的内容和日期，对产品的投入产出负责。根据市场变化对需要开发的功能排列优先顺序，合理调整产品功能和迭代顺序。Product Owner （PO）PO是了解客户需求以及相关商业价值的团队角色，作为客户代表，定义产品功能，决定产品发布的内容和日期，对产品的投入产出负责。根据市场变化对需要开发的功能排列优先顺序，合理调整产品功能和迭代顺序。</span></font>
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="项目背景" ID="ID_967345139" CREATED="1666578694891" MODIFIED="1666578700534"/>
+<node TEXT="项目目标、里程碑计划" ID="ID_1373020455" CREATED="1666678754911" MODIFIED="1666678809589"/>
+<node TEXT="项目组织结构、人员、投入比例" ID="ID_1638235261" CREATED="1666578711228" MODIFIED="1667460510572"/>
+<node TEXT="Product Backlog、业务价值、优先级" ID="ID_1119522300" CREATED="1666578700860" MODIFIED="1666679205579"/>
+<node TEXT="沟通方式、决策机制、求助方式" ID="ID_1916472580" CREATED="1666578748147" MODIFIED="1666680349409"/>
+</node>
+<node TEXT="Project Manager" FOLDED="true" ID="ID_939231611" CREATED="1666678930249" MODIFIED="1666680361661">
+<node TEXT="版本计划" ID="ID_184279557" CREATED="1666678964647" MODIFIED="1666680125454"/>
+<node TEXT="技术框架、开发工具、编码规范" ID="ID_76810679" CREATED="1666679001845" MODIFIED="1666679255128"/>
+<node TEXT="研发模式" ID="ID_1180100019" CREATED="1666578725955" MODIFIED="1666578743485">
+<node TEXT="瀑布" ID="ID_701184461" CREATED="1666681821615" MODIFIED="1667268659949">
+<icon BUILTIN="unchecked"/>
+</node>
+<node TEXT="敏捷" ID="ID_766492994" CREATED="1666681833735" MODIFIED="1667268664913">
+<icon BUILTIN="checked"/>
+</node>
+</node>
+</node>
+<node TEXT="Team" ID="ID_389088924" CREATED="1666679095044" MODIFIED="1666679100841">
+<node TEXT="Release Backlog、优先级" ID="ID_1065155657" CREATED="1666679109512" MODIFIED="1666679321656"/>
+<node TEXT="迭代计划" ID="ID_1053057324" CREATED="1666679492750" MODIFIED="1666680152277">
+<node TEXT="设计计划" ID="ID_1717277476" CREATED="1666680173130" MODIFIED="1666680177309"/>
+<node TEXT="开发计划" ID="ID_1708141434" CREATED="1666680177675" MODIFIED="1666680183858"/>
+<node TEXT="测试计划" ID="ID_1030481120" CREATED="1666680184229" MODIFIED="1666680186348"/>
+</node>
+<node TEXT="团队纪律" ID="ID_504595055" CREATED="1666578938108" MODIFIED="1666578951533">
+<node TEXT="工作纪律" ID="ID_701790656" CREATED="1666681799995" MODIFIED="1666681808983"/>
+<node TEXT="人员备份" ID="ID_621748811" CREATED="1666681915676" MODIFIED="1666681919709"/>
+</node>
+<node TEXT="意见收集" ID="ID_1352918366" CREATED="1666680630800" MODIFIED="1666680634581">
+<node TEXT="意见建议" ID="ID_1143174160" CREATED="1666682080771" MODIFIED="1666682087814">
+<node TEXT="陈述事实" ID="ID_158671013" CREATED="1666681283032" MODIFIED="1666681296188"/>
+<node TEXT="直觉判断" ID="ID_1427114813" CREATED="1666681296777" MODIFIED="1666681302442"/>
+<node TEXT="列举优点" ID="ID_1313566162" CREATED="1666681303750" MODIFIED="1666681314356"/>
+<node TEXT="列举缺点" ID="ID_1432057918" CREATED="1666681323821" MODIFIED="1666681328989"/>
+<node TEXT="提出建议" ID="ID_622697347" CREATED="1666681329323" MODIFIED="1666681333561"/>
+<node TEXT="给出意见或策略" ID="ID_1689298411" CREATED="1666681334037" MODIFIED="1666681350007"/>
+</node>
+<node TEXT="目标、关键措施" ID="ID_356655638" CREATED="1666682103906" MODIFIED="1666682119761"/>
+</node>
+</node>
+<node TEXT="QA" ID="ID_658612021" CREATED="1666678901010" MODIFIED="1666678904541">
+<node TEXT="质量度量" FOLDED="true" ID="ID_420882041" CREATED="1666578755547" MODIFIED="1667460518354" VGAP_QUANTITY="3 pt">
+<node TEXT="代码开发" ID="ID_1583475242" CREATED="1666677113051" MODIFIED="1666677120664">
+<node TEXT="设计评审缺陷率" ID="ID_1369401696" CREATED="1666677168777" MODIFIED="1666679443932">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="黑盒覆盖率" ID="ID_1850797529" CREATED="1666677121128" MODIFIED="1666677136555"/>
+<node TEXT="代码自测问题指标" ID="ID_265597011" CREATED="1666677149409" MODIFIED="1666677271803"/>
+<node TEXT="代码Review问题指标" ID="ID_545345249" CREATED="1666677248176" MODIFIED="1666677277931"/>
+<node TEXT="代码缺陷率" ID="ID_989740703" CREATED="1666677208553" MODIFIED="1666679451552">
+<icon BUILTIN="checked"/>
+</node>
+</node>
+<node TEXT="版本管理" ID="ID_164340168" CREATED="1666677304970" MODIFIED="1667460518353">
+<node TEXT="版本不良率" ID="ID_1103233283" CREATED="1666677319152" MODIFIED="1666679467297">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="缺陷解决率" ID="ID_1697982568" CREATED="1666681554327" MODIFIED="1666681767332">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="缺陷遗留DI" ID="ID_357616233" CREATED="1666681565409" MODIFIED="1666681770162">
+<icon BUILTIN="checked"/>
+</node>
+</node>
+<node TEXT="测试验证" ID="ID_949066714" CREATED="1666677138329" MODIFIED="1666677144579">
+<node TEXT="用例执行率" ID="ID_1917335188" CREATED="1666677293217" MODIFIED="1666679456281">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="用例通过率" ID="ID_1705543485" CREATED="1666677373712" MODIFIED="1666679458738">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="用例自动化率" ID="ID_994664066" CREATED="1666677448952" MODIFIED="1666677453626"/>
+<node TEXT="非用例发现缺陷比例" ID="ID_766485455" CREATED="1666677376995" MODIFIED="1666677431562"/>
+<node TEXT="自动化发现缺陷比例" ID="ID_989119721" CREATED="1666677397145" MODIFIED="1666677403908"/>
+</node>
+</node>
+<node TEXT="质量评估" ID="ID_825060714" CREATED="1666680420964" MODIFIED="1666680423790">
+<node TEXT="合同" ID="ID_1536551587" CREATED="1666680426211" MODIFIED="1666680455388">
+<node TEXT="需求满足度" ID="ID_1968397009" CREATED="1666680456546" MODIFIED="1666680462098"/>
+</node>
+<node TEXT="质量" ID="ID_619264556" CREATED="1666680470386" MODIFIED="1666680497810">
+<node TEXT="特性/功能评价" ID="ID_1762430189" CREATED="1666680498675" MODIFIED="1666680527460"/>
+<node TEXT="研发过程评估" ID="ID_1329542451" CREATED="1666680561286" MODIFIED="1666680570837"/>
+<node TEXT="实验局评估" ID="ID_1420115913" CREATED="1666680572566" MODIFIED="1666680577276"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
