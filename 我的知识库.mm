37f7e8d0008828e770231ba25d9f83a283212ee4
Jun4488 <96706340+Jun4488@users.noreply.github.com>
2023-03-16 16:45:04 +0800

    增加文件，培训和总结的入口
    增加文件，培训和总结的入口

---
+++ 我的知识库.mm
@@ -0,0 +1,146 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="知识库" FOLDED="false" ID="ID_1090958577" CREATED="1606664858024" MODIFIED="1675673929689" VGAP_QUANTITY="3 pt" COLOR="#000000" BACKGROUND_COLOR="#ccccff">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<hook NAME="MapStyle" background="#fbf1c7">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_gruvbox_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_7974122" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#3c3836" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_7974122" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#93a1a1" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#cc241d" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc241d"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_103960811" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc241d" TRANSPARENCY="255" DESTINATION="ID_103960811"/>
+<font SIZE="12" ITALIC="false"/>
+<edge COLOR="#cc241d"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#fdf6e3" BACKGROUND_COLOR="#282828" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt" BORDER_WIDTH="3.1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#282828" BACKGROUND_COLOR="#fbf1c7" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR="#2c2b29">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#282828" BACKGROUND_COLOR="#ebdbb2" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#282828" BACKGROUND_COLOR="#d79921" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#458588" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#ffffff" BACKGROUND_COLOR="#b16286" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#ffffff" BACKGROUND_COLOR="#689d6a" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#ffffff" BACKGROUND_COLOR="#a89984" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#ebdbb2" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#ebdbb2">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" BORDER_COLOR="#f0f0f0" BACKGROUND_COLOR="#98971a">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="obsidian" FOLDED="true" POSITION="right" ID="ID_1003735388" CREATED="1675670026338" MODIFIED="1675673918816" COLOR="#000000">
+<node TEXT="知识演进" ID="ID_1283375736" CREATED="1675670036497" MODIFIED="1675735566029">
+<node TEXT="图_知识形成.png" ID="ID_992287878" CREATED="1675670148556" MODIFIED="1675670148556">
+<hook URI="我的知识库_files/图_知识形成.png" SIZE="0.5876592" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="工具使用" ID="ID_375498362" CREATED="1675735364267" MODIFIED="1675735575363">
+<node TEXT="回归 Obsidian 的纯与真，写给普通人的入门指南" ID="ID_189739733" CREATED="1675671366201" MODIFIED="1675735505949" LINK="http://neo.sspai.com/post/72697">
+<font SIZE="10"/>
+</node>
+<node TEXT="如何安装Obsidian插件 | Obsidian实践" ID="ID_1703416743" CREATED="1675735366818" MODIFIED="1675735511515" LINK="https://zhuanlan.zhihu.com/p/571639575">
+<font SIZE="10"/>
+</node>
+<node TEXT="MarkDown超级教程 Obsidian版_11.4" ID="ID_1060069384" CREATED="1675735600107" MODIFIED="1675735644656" LINK="https://blog.csdn.net/qq_57187936/article/details/123923454">
+<font SIZE="10"/>
+</node>
+</node>
+</node>
+<node TEXT="流程规范" POSITION="left" ID="ID_166890993" CREATED="1675753813643" MODIFIED="1677113738650">
+<node TEXT="测试部质量评审" ID="ID_462026867" CREATED="1677113745780" MODIFIED="1677113753663">
+<node TEXT="《质量管理_测试部TTF评审》" ID="ID_1776085986" CREATED="1675753820476" MODIFIED="1677113785998" LINK="../../C:/Users/ZCG04000329/Documents/WXWork/1688855348275306/WeDrive/浩瀚深度/测试部/内部培训/流程规范_测试TTF评审.mm"/>
+<node TEXT="《质量管理_产品测试方案模板及评审" ID="ID_1052364452" CREATED="1677113825244" MODIFIED="1677113972108" LINK="../../C:/Users/ZCG04000329/Documents/WXWork/1688855348275306/WeDrive/浩瀚深度/测试部/测试设计/模板_xx产品测试方案_v1.0.mm"/>
+<node TEXT="《质量管理_产品测试报告模板及评审》" ID="ID_788023284" CREATED="1677113979340" MODIFIED="1677114002912"/>
+</node>
+<node TEXT="版本发布评审" ID="ID_1384148139" CREATED="1675909160867" MODIFIED="1677114009063">
+<node TEXT="产品部经理审批" FOLDED="true" ID="ID_1423491581" CREATED="1675909197874" MODIFIED="1675909209434">
+<node TEXT="测试部TTF评审结论" ID="ID_653975302" CREATED="1675909167241" MODIFIED="1675909232682"/>
+<node TEXT="QA评审结论" ID="ID_1227424946" CREATED="1675909179382" MODIFIED="1675909186289"/>
+<node TEXT="版本评审结论" ID="ID_517294766" CREATED="1675909269825" MODIFIED="1675909274134"/>
+</node>
+<node TEXT="技术中心经理审批" FOLDED="true" ID="ID_54254800" CREATED="1675909241182" MODIFIED="1675909247969">
+<node TEXT="版本评审结论" ID="ID_256862569" CREATED="1675909250004" MODIFIED="1675909266150"/>
+</node>
+</node>
+<node TEXT="举例" FOLDED="true" ID="ID_789381133" CREATED="1677221967186" MODIFIED="1677221969562">
+<node TEXT="研发流程_DefinitionofDone.png" ID="ID_992995759" CREATED="1677222014789" MODIFIED="1677222014789">
+<hook URI="我的知识库_files/研发流程_DefinitionofDone.png" SIZE="0.5555556" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT="培训" POSITION="left" ID="ID_658959421" CREATED="1675910104016" MODIFIED="1675910105614">
+<node TEXT="产品经理" ID="ID_1119971110" CREATED="1675992820924" MODIFIED="1676352214632">
+<node TEXT="产品经理虚拟团队" ID="ID_1907858702" CREATED="1675992826780" MODIFIED="1675992837222"/>
+<node TEXT="培训教材来源" ID="ID_154283268" CREATED="1675992837516" MODIFIED="1675992845014"/>
+</node>
+<node TEXT="测试部" ID="ID_100672115" CREATED="1676352204002" MODIFIED="1676352206672">
+<node TEXT="测试知识" ID="ID_1166636580" CREATED="1675673988541" MODIFIED="1675909149579" LINK="../../C:/Users/ZCG04000329/Documents/WXWork/1688855348275306/WeDrive/浩瀚深度/测试部/内部培训/培训_测试知识.mm"/>
+<node TEXT="《新入职员工培训》" ID="ID_1478702112" CREATED="1675910221724" MODIFIED="1676352199919" LINK="../../C:/Users/ZCG04000329/Documents/WXWork/1688855348275306/WeDrive/浩瀚深度/测试部/内部培训/培训_新员工入职培训.mm"/>
+</node>
+</node>
+<node TEXT="团队管理" POSITION="left" ID="ID_476340107" CREATED="1676335806819" MODIFIED="1676335809336">
+<node TEXT="测试团队" ID="ID_657691256" CREATED="1676334918801" MODIFIED="1676335816092" LINK="招聘/团队管理_测试.mm"/>
+</node>
+<node TEXT="风险库" POSITION="right" ID="ID_287313415" CREATED="1675753837636" MODIFIED="1675992813781"><richcontent TYPE="NOTE" CONTENT-TYPE="xml/">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      收集版本研发过程中的已知风险和未知风险、应对措施
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</map>
